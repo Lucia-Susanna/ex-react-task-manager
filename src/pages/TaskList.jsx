572bb38ac3b50c55e68f1bf9ca70114db2ab90a7
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-06-04 21:02:34 +0200

    feat: milestone 11

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,23 +1,53 @@
 import { useGlobalContext } from "../context/GlobalContext"
+import { useState, useMemo } from "react"
 import TaskRow from "../components/TaskRow"
-const TaskList = () => {
 
-    const {
-        tasks
-    } = useGlobalContext()
+const TaskList = () => {
+    const [sortBy, setSortBy] = useState('createdAt')
+    const [sortOrder, setSortOrder] = useState(1)
+
+    const { tasks } = useGlobalContext()
+
+    const handleSort = (field) => {
+        if (sortBy === field) {
+            setSortOrder(prev => prev * -1) // inverti l'ordine
+        } else {
+            setSortBy(field)
+            setSortOrder(1)
+        }
+    }
+
+    const sortedTasks = useMemo(() => {
+        return [...tasks].sort((a, b) => {
+            let comparison = 0
+
+            if (sortBy === 'title') {
+                comparison = a.title.localeCompare(b.title)
+            } else if (sortBy === 'status') {
+                const statusOrder = ['To do', 'Doing', 'Done']
+                comparison = statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status)
+            } else if (sortBy === 'createdAt') {
+                comparison = new Date(a.createdAt) - new Date(b.createdAt)
+            }
+
+            return comparison * sortOrder
+        })
+    }, [tasks, sortBy, sortOrder])
 
     return (
         <div>
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Stato</th>
-                        <th>Data di Creazione</th>
+                        <th onClick={() => handleSort('title')}>Nome</th>
+                        <th onClick={() => handleSort('status')}>Stato</th>
+                        <th onClick={() => handleSort('createdAt')}>Data di Creazione</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {tasks.map((task) => <TaskRow key={task.id} item={task} />)}
+                    {sortedTasks.map((task) => (
+                        <TaskRow key={task.id} item={task} />
+                    ))}
                 </tbody>
             </table>
         </div>
